dbfce4a00b53b3e63c965a86d59a379b816ec6f7
MuFe <1yuanfeng>
2014-09-10 09:27:04 +0800

    CreateProject

---
+++ 仿大众点评/仿大众点评/DPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DPAppDelegate.h
+//  仿大众点评
+//
+//  Created by Carl_Huang on 14-9-9.
+//  Copyright (c) 2014年 Vison. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 仿大众点评/仿大众点评/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  仿大众点评
+//
+//  Created by Carl_Huang on 14-9-9.
+//  Copyright (c) 2014年 Vison. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DPAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DPAppDelegate class]));
+    }
+}
