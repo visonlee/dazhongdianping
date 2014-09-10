2a70448ad0e0b61048b73733b3b8815b0457ec20
MuFe <1yuanfeng>
2014-09-10 17:00:30 +0800

    sync

---
+++ 仿大众点评/仿大众点评/Main/Controller/DPMainController.h
@@ -0,0 +1,13 @@
+//
+//  DPMainController.h
+//  仿大众点评
+//
+//  Created by Carl_Huang on 14-9-10.
+//  Copyright (c) 2014年 Vison. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DPMainController : UIViewController
+
+@end
---
+++ 仿大众点评/仿大众点评/Main/Controller/DPMainController.m
@@ -0,0 +1,49 @@
+//
+//  DPMainController.m
+//  仿大众点评
+//
+//  Created by Carl_Huang on 14-9-10.
+//  Copyright (c) 2014年 Vison. All rights reserved.
+//
+
+#import "DPMainController.h"
+
+@interface DPMainController ()
+
+@end
+
+@implementation DPMainController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
